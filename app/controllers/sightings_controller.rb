f4d383b3ecbff33fd72b3e062844ed35fa147d12
darren9897 <darrenkoomson17@gmail.com>
2020-07-04 11:33:43 -0400

    Done.

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,5 +1,9 @@
 class SightingsController < ApplicationController
     def show
         sighting = Sighting.find_by(id: params[:id])
-        render json: { id: sighting.id, bird: sighting.bird, location: sighting.location }      end
+        render json: sighting.to_json(:include => {
+          :bird => {:only => [:name, :species]},
+          :location => {:only => [:latitude, :longitude]}
+        }, :except => [:updated_at])
+    end
 end
